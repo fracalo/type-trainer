ba5a0841dc1e1472ae0e3657c91bce703c44ab1f
Francesco Calò <fcaloisme@gmail.com>
2020-05-25 00:14:58 +0200

    Cleanup

--- src/main.py
+++ src/main.py
@@ -1,26 +1,11 @@
 import argparse
 import tempfile
-from .game import game
-from .score import getScoreboardForCurrentTest 
-from .setUp import config
 from .db import DB
-from .testConfig import createTest, deleteTest, showTests, chooseTest
 from .narrative import Narrative
 
 ##########################################################
 # in main we parse all the arguments and decide what to do
 parser = argparse.ArgumentParser(description='A python software for improving your typing skills')
-parser.add_argument(
-        '--score',
-        dest='main',
-        action='store_const',
-        const='score',
-        help='sum the integers (default: find the max)')
-
-parser.add_argument(
-        '-w', '--wtf',
-        help='What is it that you\'re doing')
-
 parser.add_argument(
         '-d', '--db',
         help='the sqlite3 db path (if none is configured we\'ll use the default)')
@@ -29,21 +14,10 @@ parser.add_argument(
 args = parser.parse_args()
 
 
-mainLogic = {
-        'score': getScoreboardForCurrentTest,
-        'play': game,
-        'createTest': createTest,
-        'chooseTest': chooseTest,
-        'deleteTest': deleteTest,
-        'showTests': showTests,
-        'config': config,
-        None: Narrative
-    }
 
 tempDir = tempfile.gettempdir()
 dbPath = tempDir + '/test_db_trainer.db' if not args.db else args.db
 db = DB({'db': dbPath})
-action = mainLogic[args.wtf]
 
 def main():
-    action(db)
+    Narrative(db)
--- src/score.py
+++ src/score.py
@@ -29,16 +29,6 @@ class Scoreboard():
 
 
 
-def getScoreboardForCurrentTest(db):
-    info = db.getInfo()
-
-    results = db.getTestResult(info.selectedTest)
-    for result in results:
-        d = datetime.datetime.fromtimestamp(result['startedAt'])
-        formattedTime = d.strftime('%Y-%m-%d %H:%M:%S')
-
-        print('{} - {}'.format(formattedTime, result['duration']))
-
 
 
 
